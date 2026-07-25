a26cf802059d04ac07c7b9a0773f389945228085
Rahul <rahulvishwakarma13211@gmail.com>
2026-07-26 00:13:04 +0530

    Convert Counter From Class Component to Functional Component

--- counter/src/components/Counter/index.jsx
+++ counter/src/components/Counter/index.jsx
@@ -1,43 +1,49 @@
-import { Component } from 'react'
+import { useState } from "react";
+import '../../App.css';
 
-import '../../App.css'
+const Counter = () => {
+    const [count, setCount] = useState(0);
 
-class Counter extends Component {
-    state = { count: 0 }
-    onIncrement = () => {
-        this.setState(prevState => {
-            return { count: prevState.count + 1 }
-        })
+    const onIncrement = () => {
+        setCount(prevCount => prevCount + 1)
+    };
+
+    const onDecrement = () => {
+        setCount(prevCount => prevCount - 1)
     }
 
-    onDescrement = () => {
-        this.setState(prevState => ({ count: prevState.count - 1 }))
+    const onReset = () => {
+        setCount(prevCount => prevCount = 0)
     }
 
-    onReset = () => {
-        this.setState(prevState => ({ count: (prevState.count = 0) }))
-    }
+    return (
+        <div className="counter-container">
+            <h1 className="counter-title">Counter</h1>
+            <p className="counter-display">{count}</p>
+            <div>
+                <button
+                    className="button btn-increment"
+                    onClick={onIncrement}
+                >
+                    Increase
+                </button>
 
-    render() {
-        const { count } = this.state
-        return (
-            <div className="counter-container">
-                <h1 className="counter-title">Counter</h1>
-                <p className="counter-display">{count}</p>
-                <div className="">
-                    <button className="button btn-increment" onClick={this.onIncrement}>
-                        Increase
-                    </button>
-                    <button className="button btn-reset" onClick={this.onReset}>
-                        Reset
-                    </button>
-                    <button className="button btn-decrement" onClick={this.onDescrement}>
-                        Descrease
-                    </button>
-                </div>
+                <button
+                    className="button btn-reset"
+                    onClick={onReset}
+                >
+                    Reset
+                </button>
+
+                <button
+                    className="button btn-decrement"
+                    onClick={onDecrement}
+                >
+                    Decrease
+                </button>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
-export default Counter
+export default Counter;
